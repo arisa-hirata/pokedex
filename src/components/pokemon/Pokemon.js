1cd5a4e0acbccf3450d193be84a217a5bfeeb8dc
“arisa-hirata” <arisa.pirata@gmail.com>
2019-11-03 14:59:10 -0800

    fetched status data for each pokemon from the API

--- src/components/pokemon/Pokemon.js
+++ src/components/pokemon/Pokemon.js
@@ -2,7 +2,175 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 export default class Pokemon extends Component {
+  state = {
+    name: '',
+    pokemonIndex: '',
+    imageUrl: '',
+    types: [],
+    description: '',
+    stats: {
+      hp: '',
+      attack: '',
+      defense: '',
+      speed: '',
+      specialAttack: '',
+      specialDefense: ''
+    },
+    // height: '',
+    // weight: '',
+    feet: '',
+    meter: '',
+    pounds: '',
+    kg: '',
+    eggGroup: '',
+    abilities: '',
+    genderRationMale: '',
+    genderRationFemale: '',
+    evs: '',
+    hatchSteps: ''
+  };
+
+  async componentDidMount() {
+    const { pokemonIndex } = this.props.match.params;
+
+    // Urls for pokemon information
+    const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonIndex}/`;
+    const pokemonSpeciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${pokemonIndex}/`;
+
+    const pokemonRes = await axios.get(pokemonUrl);
+
+    const name = pokemonRes.data.name;
+    const imageUrl = pokemonRes.data.sprites.front_default;
+
+    let { hp, attack, defense, speed, specialAttack, specialDefense } = '';
+
+    pokemonRes.data.stats.map(stat => {
+      switch (stat.stat.name) {
+        case 'hp':
+          hp = stat['base_stat'];
+          break;
+        case 'attack':
+          attack = stat['base_stat'];
+          break;
+        case 'defense':
+          defense = stat['base_stat'];
+          break;
+        case 'speed':
+          speed = stat['base_stat'];
+          break;
+        case 'special-attack':
+          specialAttack = stat['base_stat'];
+          break;
+        case 'special-defense':
+          specialDefense = stat['base_stat'];
+          break;
+      }
+    });
+
+    // Convert Decimeters to Feet & meter
+    const feet =
+      Math.round((pokemonRes.data.height * 0.328084 + 0.0001) * 100) / 100;
+    const meter = Math.round(pokemonRes.data.height / 10);
+
+    // Convert Decimeters to 1bs & kg
+    const pounds =
+      Math.round((pokemonRes.data.weight * 0.220462 + 0.0001) * 100) / 100;
+    const kg = Math.round(pokemonRes.data.weight / 10);
+
+    const types = pokemonRes.data.types.map(type => type.type.name);
+
+    const abilities = pokemonRes.data.abilities.map(ability => {
+      return ability.ability.name
+        .toLowerCase()
+        .split('-')
+        .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+        .join(' ');
+    });
+
+    const evs = pokemonRes.data.stats
+      .filter(stat => {
+        if (stat.effort > 0) {
+          return true;
+        }
+        return false;
+      })
+      .map(stat => {
+        return `${stat.effort}${stat.stat.name}`
+          .toLowerCase()
+          .split('-')
+          .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+          .join(' ');
+      })
+      .join(', ');
+
+    // Get Pokemon Description, Catch Rate, EggGroup, Gender Ratio, Hatch Steps
+
+    await axios.get(pokemonSpeciesUrl).then(res => {
+      let description = '';
+      res.data.flavor_text_entries.some(flavor => {
+        if (flavor.language.name === 'en') {
+          description = flavor.flavor_text;
+          return;
+        }
+      });
+
+      const femaleRate = res.data['gender_rate'];
+      const genderRaioFemale = 12.5 * femaleRate;
+      const genderRatioMale = 12.5 * (8 - femaleRate);
+
+      const catchRate = Math.round((100 / 255) * res.data['capture_rate']);
+
+      const eggGroups = res.data['egg_groups']
+        .map(group => {
+          return group.name
+            .toLowerCase()
+            .split('-')
+            .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+            .join(' ');
+        })
+        .join(', ');
+
+      const hatchSteps = 255 * (res.data['hatch_counter'] + 1);
+
+      this.setState({
+        description,
+        genderRaioFemale,
+        genderRatioMale,
+        catchRate,
+        eggGroups,
+        hatchSteps
+      });
+    });
+
+    this.setState({
+      imageUrl,
+      pokemonIndex,
+      name,
+      types,
+      stats: {
+        hp,
+        attack,
+        defense,
+        speed,
+        specialAttack,
+        specialDefense
+      },
+      // height,
+      // weight,
+      feet,
+      meter,
+      pounds,
+      kg,
+      abilities,
+      evs
+    });
+  }
+
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <h1>{this.state.name}</h1>
+      </div>
+    );
   }
 }
