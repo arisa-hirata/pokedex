1094f14932b59b0a020160e3da883a68b2d0b00d
“arisa-hirata” <arisa.pirata@gmail.com>
2019-11-03 16:18:10 -0800

    displayed all fetched pokemon status data in Dashboard.js

--- src/components/pokemon/Pokemon.js
+++ src/components/pokemon/Pokemon.js
@@ -1,5 +1,26 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import Axios from 'axios';
+
+const TYPE_COLORS = {
+  bug: 'B1C12E',
+  dark: '4F3A2D',
+  dragon: '755EDF',
+  electric: 'FCBC17',
+  fairy: 'F4B1F4',
+  fighting: '823551D',
+  fire: 'E73B0C',
+  flying: 'A3B3F7',
+  ghost: '6060B2',
+  grass: '74C236',
+  ground: 'D3B357',
+  ice: 'A3E7FD',
+  normal: 'C8C4BC',
+  poison: '934594',
+  psychic: 'ED4882',
+  rock: 'B9A156',
+  steel: 'B5B5C3',
+  water: '3295F6'
+};
 
 export default class Pokemon extends Component {
   state = {
@@ -8,6 +29,8 @@ export default class Pokemon extends Component {
     imageUrl: '',
     types: [],
     description: '',
+    statTitleWidth: 3,
+    statBarWidth: 9,
     stats: {
       hp: '',
       attack: '',
@@ -16,18 +39,18 @@ export default class Pokemon extends Component {
       specialAttack: '',
       specialDefense: ''
     },
-    // height: '',
-    // weight: '',
     feet: '',
     meter: '',
     pounds: '',
     kg: '',
-    eggGroup: '',
+    eggGroups: '',
+    catchRate: '',
     abilities: '',
-    genderRationMale: '',
-    genderRationFemale: '',
+    genderRatioMale: '',
+    genderRatioFemale: '',
     evs: '',
-    hatchSteps: ''
+    hatchSteps: '',
+    themeColor: '#EF5350'
   };
 
   async componentDidMount() {
@@ -37,7 +60,8 @@ export default class Pokemon extends Component {
     const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonIndex}/`;
     const pokemonSpeciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${pokemonIndex}/`;
 
-    const pokemonRes = await axios.get(pokemonUrl);
+    // Get Pokemon Information
+    const pokemonRes = await Axios.get(pokemonUrl);
 
     const name = pokemonRes.data.name;
     const imageUrl = pokemonRes.data.sprites.front_default;
@@ -64,28 +88,33 @@ export default class Pokemon extends Component {
         case 'special-defense':
           specialDefense = stat['base_stat'];
           break;
+        default:
+          break;
       }
     });
 
-    // Convert Decimeters to Feet & meter
+    // Convert Decimeters to Feet... The + 0.0001 * 100 ) / 100 is for rounding to two decimal places :)
     const feet =
       Math.round((pokemonRes.data.height * 0.328084 + 0.0001) * 100) / 100;
-    const meter = Math.round(pokemonRes.data.height / 10);
+    const meter = Math.round(pokemonRes.data.height) / 10;
 
-    // Convert Decimeters to 1bs & kg
     const pounds =
       Math.round((pokemonRes.data.weight * 0.220462 + 0.0001) * 100) / 100;
-    const kg = Math.round(pokemonRes.data.weight / 10);
+    const kg = Math.round(pokemonRes.data.weight) / 10;
 
     const types = pokemonRes.data.types.map(type => type.type.name);
 
-    const abilities = pokemonRes.data.abilities.map(ability => {
-      return ability.ability.name
-        .toLowerCase()
-        .split('-')
-        .map(s => s.charAt(0).toUpperCase() + s.substring(1))
-        .join(' ');
-    });
+    const themeColor = `${TYPE_COLORS[types[types.length - 1]]}`;
+
+    const abilities = pokemonRes.data.abilities
+      .map(ability => {
+        return ability.ability.name
+          .toLowerCase()
+          .split('-')
+          .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+          .join(' ');
+      })
+      .join(', ');
 
     const evs = pokemonRes.data.stats
       .filter(stat => {
@@ -95,17 +124,16 @@ export default class Pokemon extends Component {
         return false;
       })
       .map(stat => {
-        return `${stat.effort}${stat.stat.name}`
+        return `${stat.effort} ${stat.stat.name
           .toLowerCase()
           .split('-')
           .map(s => s.charAt(0).toUpperCase() + s.substring(1))
-          .join(' ');
+          .join(' ')}`;
       })
       .join(', ');
 
-    // Get Pokemon Description, Catch Rate, EggGroup, Gender Ratio, Hatch Steps
-
-    await axios.get(pokemonSpeciesUrl).then(res => {
+    // Get Pokemon Description .... Is from a different end point uggh
+    await Axios.get(pokemonSpeciesUrl).then(res => {
       let description = '';
       res.data.flavor_text_entries.some(flavor => {
         if (flavor.language.name === 'en') {
@@ -113,9 +141,8 @@ export default class Pokemon extends Component {
           return;
         }
       });
-
       const femaleRate = res.data['gender_rate'];
-      const genderRaioFemale = 12.5 * femaleRate;
+      const genderRatioFemale = 12.5 * femaleRate;
       const genderRatioMale = 12.5 * (8 - femaleRate);
 
       const catchRate = Math.round((100 / 255) * res.data['capture_rate']);
@@ -124,7 +151,7 @@ export default class Pokemon extends Component {
         .map(group => {
           return group.name
             .toLowerCase()
-            .split('-')
+            .split(' ')
             .map(s => s.charAt(0).toUpperCase() + s.substring(1))
             .join(' ');
         })
@@ -134,7 +161,7 @@ export default class Pokemon extends Component {
 
       this.setState({
         description,
-        genderRaioFemale,
+        genderRatioFemale,
         genderRatioMale,
         catchRate,
         eggGroups,
@@ -155,8 +182,7 @@ export default class Pokemon extends Component {
         specialAttack,
         specialDefense
       },
-      // height,
-      // weight,
+      themeColor,
       feet,
       meter,
       pounds,
@@ -168,8 +194,301 @@ export default class Pokemon extends Component {
 
   render() {
     return (
-      <div>
-        <h1>{this.state.name}</h1>
+      <div className="col">
+        <div className="card">
+          <div className="card-header">
+            <div className="row">
+              <div className="col-2">
+                <h5># {this.state.pokemonIndex}</h5>
+              </div>
+              <div className="col-5">
+                <h3>
+                  {this.state.name
+                    .toLowerCase()
+                    .split(' ')
+                    .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+                    .join(' ')}
+                </h3>
+              </div>
+              <div className="col-5">
+                <div className="float-right">
+                  Type: &nbsp;
+                  {this.state.types.map(type => (
+                    <span
+                      key={type}
+                      className="badge badge-pill mr-1"
+                      style={{
+                        backgroundColor: `#${TYPE_COLORS[type]}`,
+                        color: 'white'
+                      }}
+                    >
+                      {type
+                        .toLowerCase()
+                        .split(' ')
+                        .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+                        .join(' ')}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="card-body">
+            <div className="row align-items-center">
+              <div className=" col-md-3 ">
+                <img
+                  src={this.state.imageUrl}
+                  className="card-img-top rounded mx-auto mt-2"
+                />
+              </div>
+              <div className="col-md-9">
+                {/* <h4 className="mx-auto">
+                  {this.state.name
+                    .toLowerCase()
+                    .split(' ')
+                    .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+                    .join(' ')}
+                </h4> */}
+                <div className="row align-items-center">
+                  <div className={`col-12 col-md-${this.state.statTitleWidth}`}>
+                    HP
+                  </div>
+                  <div className={`col-12 col-md-${this.state.statBarWidth}`}>
+                    <div className="progress">
+                      <div
+                        className="progress-bar "
+                        role="progressbar"
+                        style={{
+                          width: `${this.state.stats.hp}%`,
+                          backgroundColor: `#${this.state.themeColor}`
+                        }}
+                        aria-valuenow="25"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      >
+                        <small>{this.state.stats.hp}</small>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="row align-items-center">
+                  <div className={`col-12 col-md-${this.state.statTitleWidth}`}>
+                    Attack
+                  </div>
+                  <div className={`col-12 col-md-${this.state.statBarWidth}`}>
+                    <div className="progress">
+                      <div
+                        className="progress-bar"
+                        role="progressbar"
+                        style={{
+                          width: `${this.state.stats.attack}%`,
+                          backgroundColor: `#${this.state.themeColor}`
+                        }}
+                        aria-valuenow="25"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      >
+                        <small>{this.state.stats.attack}</small>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="row align-items-center">
+                  <div className={`col-12 col-md-${this.state.statTitleWidth}`}>
+                    Defense
+                  </div>
+                  <div className={`col-12 col-md-${this.state.statBarWidth}`}>
+                    <div className="progress">
+                      <div
+                        className="progress-bar "
+                        role="progressbar"
+                        style={{
+                          width: `${this.state.stats.defense}%`,
+                          backgroundColor: `#${this.state.themeColor}`
+                        }}
+                        aria-valuenow="25"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      >
+                        <small>{this.state.stats.defense}</small>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="row align-items-center">
+                  <div className={`col-12 col-md-${this.state.statTitleWidth}`}>
+                    Speed
+                  </div>
+                  <div className={`col-12 col-md-${this.state.statBarWidth}`}>
+                    <div className="progress">
+                      <div
+                        className="progress-bar"
+                        role="progressbar"
+                        style={{
+                          width: `${this.state.stats.speed}%`,
+                          backgroundColor: `#${this.state.themeColor}`
+                        }}
+                        aria-valuenow="25"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      >
+                        <small>{this.state.stats.speed}</small>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="row align-items-center">
+                  <div className={`col-12 col-md-${this.state.statTitleWidth}`}>
+                    Sp Atk
+                  </div>
+                  <div className={`col-12 col-md-${this.state.statBarWidth}`}>
+                    <div className="progress">
+                      <div
+                        className="progress-bar "
+                        role="progressbar"
+                        style={{
+                          width: `${this.state.stats.specialAttack}%`,
+                          backgroundColor: `#${this.state.themeColor}`
+                        }}
+                        aria-valuenow={this.state.stats.specialAttack}
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      >
+                        <small>{this.state.stats.specialAttack}</small>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="row align-items-center">
+                  <div className={`col-12 col-md-${this.state.statTitleWidth}`}>
+                    Sp Def
+                  </div>
+                  <div className={`col-12 col-md-${this.state.statBarWidth}`}>
+                    <div className="progress">
+                      <div
+                        className="progress-bar "
+                        role="progressbar"
+                        style={{
+                          width: `${this.state.stats.specialDefense}%`,
+                          backgroundColor: `#${this.state.themeColor}`
+                        }}
+                        aria-valuenow={this.state.stats.specialDefense}
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      >
+                        <small>{this.state.stats.specialDefense}</small>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="row mt-1">
+              <div className="col">
+                <p className="">{this.state.description}</p>
+              </div>
+            </div>
+          </div>
+          <hr />
+          <div className="card-body">
+            <h5 class="card-title text-center">Profile</h5>
+            <div className="row">
+              <div className="col-md-6">
+                <div className="row">
+                  <div className="col-6">
+                    <h6 className="float-right">Height:</h6>
+                  </div>
+                  <div className="col-6">
+                    <h6 className="float-left">
+                      {this.state.feet} ft. ({this.state.meter} m)
+                    </h6>
+                  </div>
+                  <div className="col-6">
+                    <h6 className="float-right">Weight:</h6>
+                  </div>
+                  <div className="col-6">
+                    <h6 className="float-left">
+                      {this.state.pounds} lbs ({this.state.kg}) kg
+                    </h6>
+                  </div>
+                  <div className="col-6">
+                    <h6 className="float-right">Catch Rate:</h6>
+                  </div>
+                  <div className="col-6">
+                    <h6 className="float-left">{this.state.catchRate}%</h6>
+                  </div>
+                  <div className="col-6">
+                    <h6 className="float-right">Gender Ratio:</h6>
+                  </div>
+                  <div className="col-6">
+                    <div class="progress">
+                      <div
+                        class="progress-bar"
+                        role="progressbar"
+                        style={{
+                          width: `${this.state.genderRatioFemale}%`,
+                          backgroundColor: '#c2185b'
+                        }}
+                        aria-valuenow="15"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      >
+                        <small>{this.state.genderRatioFemale}</small>
+                      </div>
+                      <div
+                        class="progress-bar"
+                        role="progressbar"
+                        style={{
+                          width: `${this.state.genderRatioMale}%`,
+                          backgroundColor: '#1976d2'
+                        }}
+                        aria-valuenow="30"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      >
+                        <small>{this.state.genderRatioMale}</small>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="row">
+                  <div className="col-6">
+                    <h6 className="float-right">Egg Groups:</h6>
+                  </div>
+                  <div className="col-6">
+                    <h6 className="float-left">{this.state.eggGroups} </h6>
+                  </div>
+                  <div className="col-6">
+                    <h6 className="float-right">Hatch Steps:</h6>
+                  </div>
+                  <div className="col-6">
+                    <h6 className="float-left">{this.state.hatchSteps}</h6>
+                  </div>
+                  <div className="col-6">
+                    <h6 className="float-right">Abilities:</h6>
+                  </div>
+                  <div className="col-6">
+                    <h6 className="float-left">{this.state.abilities}</h6>
+                  </div>
+                  <div className="col-6">
+                    <h6 className="float-right">EVs:</h6>
+                  </div>
+                  <div className="col-6">
+                    <h6 className="float-left">{this.state.evs}</h6>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="card-footer text-muted">
+            Data From{' '}
+            <a href="https://pokeapi.co/" target="_blank" className="card-link">
+              PokeAPI.co
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
